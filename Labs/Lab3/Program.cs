4aa4e6750effd62cc189fb8fa0bb4c1114bd7725
palamar-afk <pavelskidan2020@gmail.com>
2020-09-16 18:42:12 +0300

    lab3

--- Labs/Lab3/Program.cs
+++ Labs/Lab3/Program.cs
@@ -6,7 +6,23 @@ namespace Lab3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a, b, c;
+            Console.Write("Enter 1st number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter 2nd number: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter 3rd number: ");
+            c = Convert.ToInt32(Console.ReadLine());
+            if(a==b || a==c || b==c)
+            {
+                Console.WriteLine("There is at least a pair of the same numbers");
+            }
+            else
+            {
+                Console.WriteLine("There is not the same numbers");
+            }
+
+
         }
     }
 }
