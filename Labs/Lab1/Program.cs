af7f06092f5b484170c78bed8a7362502a2b0e84
palamar-afk <pavelskidan2020@gmail.com>
2020-09-16 17:51:16 +0300

    добавлены 2 лабы

---
+++ Labs/Lab1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter radius(separator ','): ");
+            string radius_str = Console.ReadLine();
+            double radius = Convert.ToDouble(radius_str);
+            Console.Write("Enter height(separotor ','): ");
+            string height_str = Console.ReadLine();
+            double height = Convert.ToDouble(height_str);
+            double result = Math.PI * radius * radius * height * 1 / 3;
+            Console.WriteLine("Volume of conus is " + result);
+        }
+    }
+}
---
+++ Labs/Lab2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int finish_index;
+            int start_index;
+            Console.Write("Enter start index(integer): ");
+            string start_index_str = Console.ReadLine();
+            start_index = Convert.ToInt32(start_index_str);
+            Console.Write("Enter finish index(integer): ");
+            string finish_index_str = Console.ReadLine();
+            finish_index = Convert.ToInt32(finish_index_str);
+            double summ = 0;
+            if (start_index <= finish_index && start_index >= 0)
+            {
+                for (int i = start_index; i < finish_index; i++)
+                {
+                    summ *= (i * i + Math.Pow(-1, i - 1) * 2 * i - 1) / (i * i + 8);
+                }
+                Console.WriteLine("Summ of row: " + summ);
+            }
+            else
+            {
+                Console.WriteLine("Invalid indexes. Restart and try again");
+            }
+
+        }
+    }
+}
