9139514b50a1c5a7e172fe61e70312305ced6bb0
palamar-afk <pavelskidan2019@gmail.com>
2021-06-09 20:50:53 +0300

    add lab19

---
+++ Labs15-19/Labs15_19/Lab19/DataFromWindow.cs
@@ -0,0 +1,12 @@
+﻿namespace Lab19
+{
+    class DataFromWindow
+    {
+        public bool CheckBox1 { set; get; }
+        public bool CheckBox2 { set; get; }
+        public bool CheckBox3 { set; get; }
+        public string TextFieldContent { set; get; }
+        public double ScreenWidth { set; get; }
+        public double ScreenHeight { set; get; }
+    }
+}
---
+++ Labs15-19/Labs15_19/Lab19/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+using System.Text.Json;
+using System.IO;
+
+namespace Lab19
+{
+    public partial class MainWindow : Window
+    {
+        private string fileName = @"C:\Users\Fiede\Desktop\data.json";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void WriteDataButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            DataFromWindow data = new DataFromWindow();
+            data.CheckBox1 = (bool)check1.IsChecked;
+            data.CheckBox2 = (bool)check2.IsChecked;
+            data.CheckBox3 = (bool)check3.IsChecked;
+
+            data.TextFieldContent = TextBoxContent.Text;
+
+            data.ScreenHeight = Height;
+            data.ScreenWidth = Width;
+
+            string json = JsonSerializer.Serialize(data);
+            try
+            {
+                using (StreamWriter streamWriter = new StreamWriter(fileName))
+                {
+                    string settings = JsonSerializer.Serialize(data);
+                    streamWriter.Write(settings);
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Не удалось записать файл!");
+            }
+        }
+    }
+}
